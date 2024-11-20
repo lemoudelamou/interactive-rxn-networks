6bab16add1d59210ceb8b765c65cf1e8b2d41d3f
s91072 <s91072@bht-berlin.de>
2024-11-20 16:58:53 +0100

    Revert "update utils files"
    This reverts commit 370d68321c7efd641ebcae6a44c8928a5ce22f06.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import Graph from './graph/Graph.js';
+import React, { useState } from 'react';
+import Graph from './components/Graph';
+import InputForm from './components/InputForm';
+import useReactionNetwork from './utils/UseReactionNetwork';
 import logo from './assets/logo-Haber.png'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import FileUpload from './utils/FileUpload.js';
-import Modal from './utils/Modal.js';
-import Image from 'react-bootstrap/Image';
 
-
-function App() {
-
-  const currentYear = new Date().getFullYear();
-
-  const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const handleOpen = () => setIsModalOpen(true);
-    const handleClose = () => setIsModalOpen(false);
-
+const App = () => {
+  const { nodes, edges, handleAddReaction } = useReactionNetwork();
 
   return (
-    <div className="App"> 
+    <div>
       <header className="header">
-        <Image src={logo} alt="Logo" className="logo" roundedCircle/>
-        <h1 className="title-box">Chemical Reaction Network Explorer</h1>
-        <button className="instruction-button" onClick={handleOpen}>
-                <i className="fa-regular fa-circle-question" style={{ fontSize: 30 }}></i>
-            </button>
-        <Modal className="instruction-button" open={isModalOpen} onClose={handleClose}/>
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="title-box">Chemical Reaction Network Explorer</h1>
+      <button className="instruction-button" onClick=''>
+        Instructions
+      </button>
     </header>
-    <body className="App-header">
-      <div>    
-        <FileUpload setNodes={setNodes} setEdges={setEdges} />
-      </div>
-      
-      <Graph nodesData={nodes} edgesData={edges} />
+    <body className='page-container'>
+      <InputForm onAddReaction={handleAddReaction} />
+      <Graph nodes={nodes} edges={edges} />
     </body>
     <footer>
       <div class="footer-container">
         <div class="footer-left">
-          <p>&copy; {currentYear} Fritz-Haber-Institut. All rights reserved.</p>
+          <p>&copy; 2024 Fritz-Haber-Institut. All rights reserved.</p>
         </div>
-      </div>
-    </footer>
-  </div>
+        
+    </div>
+</footer>
+
+    </div>
   );
-}
+};
 
 export default App;
