2f8d931a66480d52e1b1a0d1bfc56b170257d280
s91072 <s91072@bht-berlin.de>
2024-12-26 18:55:50 +0100

    remove graph compoenents from app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,97 @@
 import './App.css';
-import React, { useState } from 'react';
-import Graph from './graph/Graph.js';
+import React, { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import FileUpload from './utils/FileUpload.js';
-import Header from './components/Header.js'
-import Footer from './components/Footer.js'
-
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import { saveGraphData } from './api/api.js';
+import { PickleContext } from './context/FileDataContext';
+import TitleModal from './modals/SaveModal.js';
 
 function App() {
-
-  const currentYear = new Date().getFullYear();
-
+  const navigate = useNavigate();
   const [nodes, setNodes] = useState([]);
   const [edges, setEdges] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [isSaving, setIsSaving] = useState(false);
+  const [message, setMessage] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [graphTitle, setGraphTitle] = useState('MyGraph');
+  const { pickleData, freeEnergyData } = useContext(PickleContext);
 
-   
+  const handleSave = async (title) => {
+    setIsSaving(true);
+    setMessage('');
+    setGraphTitle(title);
 
+    try {
+      const responseMessage = await saveGraphData(nodes, edges, title, pickleData, freeEnergyData);
+      setMessage(`Success: ${responseMessage}`);
+    } catch (error) {
+      console.error("Error saving graph:", error.message);
+      setMessage(`Error: Failed to save graph. ${error.message}`);
+    } finally {
+      setIsSaving(false);
+      setIsModalOpen(false);
+    }
+  };
+
+  const handleViewGraph = () => {
+    navigate('/graph-page', {
+      state: {
+        edges,
+        extractedData: nodes,
+        data: pickleData?.data || [],
+        freeEnergy: freeEnergyData,
+      },
+    });
+  };
+
+  useEffect(() => {
+    const rateControlData = pickleData?.data?.rate_control || [];
+    const processedRateControlData = rateControlData.map((item) => {
+      const compound = Object.keys(item)[0];
+      const properties = Object.entries(item[compound]).map(([label, details]) => ({
+        label,
+        value: details.value,
+      }));
+      return { compound, properties };
+    });
+    setTableData(processedRateControlData);
+  }, [pickleData]);
 
   return (
-    <div className="App"> 
-        <Header/>
-      <div>    
+    <div className="App">
+      <Header />
+      <div>
         <FileUpload setNodes={setNodes} setEdges={setEdges} />
       </div>
-      
-      <Graph nodesData={nodes} edgesData={edges} />
-      <Footer/>
-  </div>
+
+      <div className="message">
+        <button className="button-style" onClick={() => setIsModalOpen(true)} disabled={isSaving}>
+          {isSaving ? "Saving..." : "Save To List"}
+        </button>
+        <button className="button-style" onClick={handleViewGraph}>
+          View Graph
+        </button>
+        {message && (
+          <p className={`message ${message.includes('Error') ? 'error' : 'success'}`}>
+            {message}
+          </p>
+        )}
+      </div>
+
+     
+
+      <TitleModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSave={handleSave}
+      />
+
+      <Footer />
+    </div>
   );
 }
 
