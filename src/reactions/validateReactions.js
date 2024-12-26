3b10740fcecac858e93626a24451b3ca9e909ded
s91072 <s91072@bht-berlin.de>
2024-12-26 20:17:24 +0100

    .

--- src/reactions/validateReactions.js
+++
@@ -1,8 +0,0 @@
-export function validateReactionData(reactionData) {
-    if (!reactionData) {
-      console.warn("No reactions data available.");
-      throw new Error("No reactions data available.");
-    }
-    return reactionData;
-  }
-  
