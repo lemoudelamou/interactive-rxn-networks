d1d73a68097bad5d76a3e639a966dd7179d5e431
s91072 <s91072@bht-berlin.de>
2025-01-22 03:04:02 +0100

    update chart

--- frontend/src/chart-card/Chart.js
+++ frontend/src/chart-card/Chart.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Line, Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, LogarithmicScale, Tooltip, Legend, LineElement, PointElement } from 'chart.js';
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+} from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(BarElement, CategoryScale, LinearScale, LogarithmicScale, Tooltip, Legend, LineElement, PointElement);
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  ChartDataLabels
+);
 
 const Chart = ({ chartData, logScale = false, isLineChart = false }) => {
   if (!chartData) return null;
@@ -24,21 +45,46 @@ const Chart = ({ chartData, logScale = false, isLineChart = false }) => {
               callbacks: {
                 label: function (tooltipItem) {
                   const value = tooltipItem.raw;
-                  return `${tooltipItem.dataset.label}: ${value.toExponential ? value.toExponential(2) : value.toFixed(2)}`;
+                  return `${tooltipItem.dataset.label}: ${
+                    value.toExponential ? value.toExponential(2) : value.toFixed(2)
+                  }`;
                 },
+              },
+            },
+            datalabels: {
+              display: true,
+              align: 'top', 
+              color: '#000', 
+              font: {
+                size: 10, 
+              },
+              formatter: function (value, context) {
+                const label = context.chart.data.labels[context.dataIndex];
+                return !isNaN(label) ? Number(label).toFixed(2) : label;
               },
             },
           },
           scales: {
+            x: {
+              grid: {
+                display: false, 
+              },
+              ticks: {
+                display: false,
+                callback: function (value) {
+                  const label = this.getLabelForValue(value);
+                  return !isNaN(label) ? Number(label).toFixed(2) : label;
+                },
+              },
+            },
             y: {
               beginAtZero: true,
               type: logScale ? 'logarithmic' : 'linear',
-              position: 'left',
               ticks: {
                 callback: function (value) {
                   return value.toExponential ? value.toExponential(2) : value.toFixed(2);
                 },
-                maxTicksLimit: 15, 
+                maxTicksLimit: 10,
               },
             },
           },
